5fc33a295ddcf168b87762e8013ccb6cbd29f167
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-04-19 14:47:18 +0300

    Auto-generated commit [ci skip]

--- src/app/search-class/repository.ts
+++
@@ -1,3 +0,0 @@
-export class Repository {
-    constructor(public html_url:string,public title:string, public about:string, public language:string){}
-}
--- src/app/search-class/user.ts
+++
@@ -1,3 +0,0 @@
-export class User {
-    constructor (public name:string, public avatar: string, public bio:string, public email:any){}
-}
